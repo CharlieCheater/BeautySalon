5cb95b05d84121cdcddea643e3956f0e66460b50
Никита Маркелов <charliecheaters@yandex.ru>
2020-11-17 19:16:04 +0300

    Добавьте файлы проекта.

---
+++ BeautySalon/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeautySalon
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private bool _isAdmin = false;
+        public bool IsAdmin
+        {
+            get => _isAdmin;
+            set
+            {
+                _isAdmin = value;
+                if (value)
+                    AdminText.Text = "Выйти из режима администрирования";
+                else 
+                    AdminText.Text = "Войти в режим администрирования";
+            }
+        }
+
+        private void AdminLink_Click(object sender, RoutedEventArgs e)
+        {
+            IsAdmin = !IsAdmin;
+        }
+    }
+}
